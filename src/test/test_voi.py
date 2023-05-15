a37189e76b8e783ac179d794abb2c5f76a244711
Binit Shah <bshah@hello-robot.com>
2023-05-15 00:28:30 -0700

    add new projection unittest

--- src/test/test_voi.py
+++ src/test/test_voi.py
@@ -58,21 +58,25 @@ class TestVOI(unittest.TestCase):
             [-0.05]
         ])
         points_wrt_map = (voi.points_in_voi_to_frame_id_mat @ np.vstack((points_wrt_voi, np.ones(points_wrt_voi.shape[1]))))[:3,:]
+        points_wrt_voi_again = (voi.points_in_frame_id_to_voi_mat @ np.vstack((points_wrt_map, np.ones(points_wrt_map.shape[1]))))[:3,:]
         np.testing.assert_array_equal(expected_points_wrt_map, points_wrt_map)
+        np.testing.assert_array_equal(points_wrt_voi, points_wrt_voi_again)
 
         # test two points
         points_wrt_voi = np.array([
-            [0.0, 0.0],
-            [0.0, 0.0],
-            [0.0, 0.0]
+            [0.0, 1.0],
+            [0.0, 1.0],
+            [0.0, 1.0]
         ])
         expected_points_wrt_map = np.array([
-            [-4.0, -4.0],
-            [-4.0, -4.0],
-            [-0.05, -0.05]
+            [-4.0, -3.0],
+            [-4.0, -3.0],
+            [-0.05, 0.95]
         ])
         points_wrt_map = (voi.points_in_voi_to_frame_id_mat @ np.vstack((points_wrt_voi, np.ones(points_wrt_voi.shape[1]))))[:3,:]
+        points_wrt_voi_again = (voi.points_in_frame_id_to_voi_mat @ np.vstack((points_wrt_map, np.ones(points_wrt_map.shape[1]))))[:3,:]
         np.testing.assert_array_equal(expected_points_wrt_map, points_wrt_map)
+        np.testing.assert_array_equal(points_wrt_voi, points_wrt_voi_again)
 
         # test three points
         points_wrt_voi = np.array([
@@ -86,7 +90,9 @@ class TestVOI(unittest.TestCase):
             [-0.05, -0.05, 0.0]
         ])
         points_wrt_map = (voi.points_in_voi_to_frame_id_mat @ np.vstack((points_wrt_voi, np.ones(points_wrt_voi.shape[1]))))[:3,:]
+        points_wrt_voi_again = (voi.points_in_frame_id_to_voi_mat @ np.vstack((points_wrt_map, np.ones(points_wrt_map.shape[1]))))[:3,:]
         np.testing.assert_array_equal(expected_points_wrt_map, points_wrt_map)
+        np.testing.assert_array_equal(points_wrt_voi, points_wrt_voi_again)
 
     def test_points_voi_transformations(self):
         """Create a VolumeOfInterest object and verify that the
@@ -110,7 +116,7 @@ class TestVOI(unittest.TestCase):
         np.testing.assert_array_equal(expected_points_to_voi_mat, voi.get_points_to_voi_matrix(np.eye(4)))
 
         # give +1m on z axis, and expect the points to voi matrix to shift points up along z an extra 1m
-        points_to_frame_mat = np.array([
+        camera_to_map_mat = np.array([
             [1.0, 0.0, 0.0, 0.0],
             [0.0, 1.0, 0.0, 0.0],
             [0.0, 0.0, 1.0, 1.0],
@@ -122,4 +128,83 @@ class TestVOI(unittest.TestCase):
             [0.0, 0.0, 1.0, 1.05],
             [0.0, 0.0, 0.0, 1.0]
         ])
-        np.testing.assert_array_equal(expected_points_to_voi_mat, voi.get_points_to_voi_matrix(points_to_frame_mat))
+        np.testing.assert_array_equal(expected_points_to_voi_mat, voi.get_points_to_voi_matrix(camera_to_map_mat))
+
+    def test_points_voi_projection(self):
+        """Create a VolumeOfInterest object and verify that points
+        projected with the "points to voi" transformation matrix from `get_points_to_voi_matrix()`
+        is correct.
+        """
+        xy_m = 8.0
+        z_m = 2.0
+        voi_side_m = xy_m
+        origin = np.array([-voi_side_m/2.0, -voi_side_m/2.0, -0.05])
+        axes = np.eye(3)
+        voi = stretch_funmap.max_height_image.VolumeOfInterest('map', origin, axes, xy_m, xy_m, z_m)
+
+        # test one point - camera frame shifted +1m on z axis
+        shift = 1.0
+        camera_to_map_mat = np.array([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, shift],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        camera_to_voi_mat = voi.get_points_to_voi_matrix(camera_to_map_mat)
+        points_wrt_camera = np.array([
+            [0.0],
+            [0.0],
+            [0.0]
+        ])
+        points_wrt_voi = (camera_to_voi_mat @ np.vstack((points_wrt_camera, np.ones(points_wrt_camera.shape[1]))))[:3,:]
+        expected_points_wrt_voi = np.array([
+            [4.0],
+            [4.0],
+            [1.05]
+        ])
+        np.testing.assert_array_equal(expected_points_wrt_voi, points_wrt_voi)
+
+        # test two points - camera frame rotated 90 degrees on z axis
+        rot = np.pi/2
+        camera_to_map_mat = np.array([
+            [np.cos(rot), -np.sin(rot), 0.0, 0.0],
+            [np.sin(rot),  np.cos(rot), 0.0, 0.0],
+            [0.0,                  0.0, 1.0, 0.0],
+            [0.0,                  0.0, 0.0, 1.0]
+        ])
+        camera_to_voi_mat = voi.get_points_to_voi_matrix(camera_to_map_mat)
+        points_wrt_camera = np.array([
+            [0.0, 1.0],
+            [0.0, 1.0],
+            [0.0, 1.0]
+        ])
+        points_wrt_voi = (camera_to_voi_mat @ np.vstack((points_wrt_camera, np.ones(points_wrt_camera.shape[1]))))[:3,:]
+        expected_points_wrt_voi = np.array([
+            [4.0, 3.0],
+            [4.0, 5.0],
+            [0.05, 1.05]
+        ])
+        np.testing.assert_array_equal(expected_points_wrt_voi, points_wrt_voi)
+
+        # test three points - camera frame rotated 90 degrees on z axis, shifted 1m along x and z axes
+        rot = np.pi/2
+        shift = 1.0
+        camera_to_map_mat = np.array([
+            [np.cos(rot), -np.sin(rot), 0.0, shift],
+            [np.sin(rot),  np.cos(rot), 0.0, 0.0],
+            [0.0,                  0.0, 1.0, shift],
+            [0.0,                  0.0, 0.0, 1.0]
+        ])
+        camera_to_voi_mat = voi.get_points_to_voi_matrix(camera_to_map_mat)
+        points_wrt_camera = np.array([
+            [0.0, 1.0, -4],
+            [0.0, 1.0, 5],
+            [0.0, 1.0, -1.05]
+        ])
+        points_wrt_voi = (camera_to_voi_mat @ np.vstack((points_wrt_camera, np.ones(points_wrt_camera.shape[1]))))[:3,:]
+        expected_points_wrt_voi = np.array([
+            [5.0, 4.0, 0.0],
+            [4.0, 5.0, 0.0],
+            [1.05, 2.05, 0.0]
+        ])
+        np.testing.assert_allclose(expected_points_wrt_voi, points_wrt_voi, atol=1e-7)
