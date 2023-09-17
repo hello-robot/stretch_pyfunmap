f6081e6df8a5cc5bf2199ab40871864d26e4e205
Binit Shah <bshah@hello-robot.com>
2023-09-17 17:46:47 -0400

    Add second get_surface performance test

--- src/test/test_segment_objects.py
+++ src/test/test_segment_objects.py
@@ -16,7 +16,7 @@ class TestSegmentObjects(unittest.TestCase):
         LOG:
          - Sept 17th, 2023 on a Stretch RE1 with ./assets/segment_objects/manip_found.pkl
         """
-        # TODO:
+        # TODO: collect enough data and log here
         # self.get_surface_exectime_ms = np.array([31.77, 29.89])
         # self.get_surface_exectime_ms_mean = np.mean(self.get_surface_exectime_ms)
         # self.get_surface_exectime_ms_std = np.std(self.get_surface_exectime_ms)
@@ -52,13 +52,29 @@ class TestSegmentObjects(unittest.TestCase):
     def test_find_surface_performance1(self):
         """Use timeit to measure the # of ms it takes to execute this
         method with observations from manip_found.pkl.
+        Expected runtime is ~5 minutes.
         """
         pkl_fpath = Path(__file__).parent / 'assets' / 'segment_objects' / 'manip_found.pkl'
         with open(str(pkl_fpath), 'rb') as inp:
             manip_found = pickle.load(inp)
 
         single_ms = self.timeit_get_surface(manip_found)
-        print(f"Get Surface: {single_ms:.2f} ms (Add your result to setUp() in test_segment_objects)")
-        # TODO: identify outlier
+        print(f"Get Surface: {single_ms:.2f} ms (Add your result to setUp() in test_segment_objects for ./assets/segment_objects/manip_found.pkl)")
+        # TODO: collect enough data to identify outliers
+        # self.assertTrue(single_ms > self.get_surface_exectime_ms_mean - 3.0 * self.get_surface_exectime_ms_std)
+        # self.assertTrue(single_ms < self.get_surface_exectime_ms_mean + 3.0 * self.get_surface_exectime_ms_std)
+
+    def test_find_surface_performance2(self):
+        """Use timeit to measure the # of ms it takes to execute this
+        method with observations from manip_two.pkl.
+        Expected runtime is ~5 minutes.
+        """
+        pkl_fpath = Path(__file__).parent / 'assets' / 'segment_objects' / 'manip_two.pkl'
+        with open(str(pkl_fpath), 'rb') as inp:
+            manip_two = pickle.load(inp)
+
+        single_ms = self.timeit_get_surface(manip_two)
+        print(f"Get Surface: {single_ms:.2f} ms (Add your result to setUp() in test_segment_objects for ./assets/segment_objects/manip_two.pkl)")
+        # TODO: collect enough data to identify outliers
         # self.assertTrue(single_ms > self.get_surface_exectime_ms_mean - 3.0 * self.get_surface_exectime_ms_std)
         # self.assertTrue(single_ms < self.get_surface_exectime_ms_mean + 3.0 * self.get_surface_exectime_ms_std)
