1dd8ab653097c1322b5f00fc98981153a4178004
Binit Shah <bshah@hello-robot.com>
2023-09-23 12:38:36 -0400

    Add performance tests for find_objects_to_grasp()

--- src/test/test_segment_objects_perf.py
+++ src/test/test_segment_objects_perf.py
@@ -24,6 +24,9 @@ class TestSegmentObjects(unittest.TestCase):
         # self.get_object_exectime_ms = np.array([60.48, 47.92])
         # self.get_object_exectime_ms_mean = np.mean(self.get_object_exectime_ms)
         # self.get_object_exectime_ms_std = np.std(self.get_object_exectime_ms)
+        # self.get_objects_exectime_ms = np.array([60.48, 47.92])
+        # self.get_objects_exectime_ms_mean = np.mean(self.get_objects_exectime_ms)
+        # self.get_objects_exectime_ms_std = np.std(self.get_objects_exectime_ms)
 
     def get_surface(self, manip):
         height, width = manip.max_height_im.image.shape
@@ -48,7 +51,7 @@ class TestSegmentObjects(unittest.TestCase):
             manip_found = pickle.load(inp)
 
         single_ms = self.timeit_get_surface(manip_found)
-        print(f"Get Surface: {single_ms:.2f} ms (Add your result to get_surface_exectime_ms in test_segment_objects.setUp() for ./assets/segment_objects/manip_found.pkl)")
+        print(f"Get Surface: {single_ms:.2f} ms (Add your result to get_surface_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_found.pkl)")
         # TODO: collect enough data to identify outliers
         # self.assertTrue(single_ms > self.get_surface_exectime_ms_mean - 3.0 * self.get_surface_exectime_ms_std)
         # self.assertTrue(single_ms < self.get_surface_exectime_ms_mean + 3.0 * self.get_surface_exectime_ms_std)
@@ -63,7 +66,7 @@ class TestSegmentObjects(unittest.TestCase):
             manip_two = pickle.load(inp)
 
         single_ms = self.timeit_get_surface(manip_two)
-        print(f"Get Surface: {single_ms:.2f} ms (Add your result to get_surface_exectime_ms in test_segment_objects.setUp() for ./assets/segment_objects/manip_two.pkl)")
+        print(f"Get Surface: {single_ms:.2f} ms (Add your result to get_surface_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_two.pkl)")
         # TODO: collect enough data to identify outliers
         # self.assertTrue(single_ms > self.get_surface_exectime_ms_mean - 3.0 * self.get_surface_exectime_ms_std)
         # self.assertTrue(single_ms < self.get_surface_exectime_ms_mean + 3.0 * self.get_surface_exectime_ms_std)
@@ -90,14 +93,14 @@ class TestSegmentObjects(unittest.TestCase):
 
         surface_mask, plane_parameters = self.get_surface(manip_found)
         single_ms = self.timeit_get_object(surface_mask, plane_parameters, manip_found, n=1e2)
-        print(f"Get Object: {single_ms:.2f} ms (Add your result to get_object_exectime_ms in test_segment_objects.setUp() for ./assets/segment_objects/manip_found.pkl)")
+        print(f"Get Object: {single_ms:.2f} ms (Add your result to get_object_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_found.pkl)")
         # TODO: collect enough data to identify outliers
         # self.assertTrue(single_ms > self.get_object_exectime_ms_mean - 3.0 * self.get_object_exectime_ms_std)
         # self.assertTrue(single_ms < self.get_object_exectime_ms_mean + 3.0 * self.get_object_exectime_ms_std)
 
     def test_find_grasp_target_performance2(self):
         """Use timeit to measure the # of ms it takes to execute this
-        method with observations from manip_found.pkl.
+        method with observations from manip_two.pkl.
         Expected runtime is ~10 minutes.
         """
         pkl_fpath = Path(__file__).parent / 'assets' / 'segment_objects' / 'manip_two.pkl'
@@ -106,7 +109,50 @@ class TestSegmentObjects(unittest.TestCase):
 
         surface_mask, plane_parameters = self.get_surface(manip_two)
         single_ms = self.timeit_get_object(surface_mask, plane_parameters, manip_two)
-        print(f"Get Object: {single_ms:.2f} ms (Add your result to get_object_exectime_ms in test_segment_objects.setUp() for ./assets/segment_objects/manip_two.pkl)")
+        print(f"Get Object: {single_ms:.2f} ms (Add your result to get_object_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_two.pkl)")
         # TODO: collect enough data to identify outliers
         # self.assertTrue(single_ms > self.get_object_exectime_ms_mean - 3.0 * self.get_object_exectime_ms_std)
         # self.assertTrue(single_ms < self.get_object_exectime_ms_mean + 3.0 * self.get_object_exectime_ms_std)
+
+    def get_objects(self, surface, plane_parameters, manip):
+        grasp_targets = sm.find_objects_to_grasp(surface, plane_parameters, manip.max_height_im)
+        return grasp_targets
+
+    def timeit_get_objects(self, surface, plane_parameters, manip, n=1e4):
+        n = int(n)
+        total = ProgressTimer(lambda: self.get_objects(surface, plane_parameters, manip)).timeit(number=n)
+        single = total / n
+        single_ms = single * 1000
+        return single_ms
+
+    def test_find_grasp_targets_performance1(self):
+        """Use timeit to measure the # of ms it takes to execute this
+        method with observations from manip_found.pkl.
+        Expected runtime is ~1 minute.
+        """
+        pkl_fpath = Path(__file__).parent / 'assets' / 'segment_objects' / 'manip_found.pkl'
+        with open(str(pkl_fpath), 'rb') as inp:
+            manip_found = pickle.load(inp)
+
+        surface_mask, plane_parameters = self.get_surface(manip_found)
+        single_ms = self.timeit_get_objects(surface_mask, plane_parameters, manip_found, n=1e2)
+        print(f"Get Objects: {single_ms:.2f} ms (Add your result to get_objects_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_found.pkl)")
+        # TODO: collect enough data to identify outliers
+        # self.assertTrue(single_ms > self.get_objects_exectime_ms_mean - 3.0 * self.get_objects_exectime_ms_std)
+        # self.assertTrue(single_ms < self.get_objects_exectime_ms_mean + 3.0 * self.get_objects_exectime_ms_std)
+
+    def test_find_grasp_targets_performance2(self):
+        """Use timeit to measure the # of ms it takes to execute this
+        method with observations from manip_two.pkl.
+        Expected runtime is ~10 minutes.
+        """
+        pkl_fpath = Path(__file__).parent / 'assets' / 'segment_objects' / 'manip_two.pkl'
+        with open(str(pkl_fpath), 'rb') as inp:
+            manip_two = pickle.load(inp)
+
+        surface_mask, plane_parameters = self.get_surface(manip_two)
+        single_ms = self.timeit_get_objects(surface_mask, plane_parameters, manip_two)
+        print(f"Get Objects: {single_ms:.2f} ms (Add your result to get_objects_exectime_ms in test_segment_objects_perf.setUp() for ./assets/segment_objects/manip_two.pkl)")
+        # TODO: collect enough data to identify outliers
+        # self.assertTrue(single_ms > self.get_objects_exectime_ms_mean - 3.0 * self.get_objects_exectime_ms_std)
+        # self.assertTrue(single_ms < self.get_objects_exectime_ms_mean + 3.0 * self.get_objects_exectime_ms_std)
