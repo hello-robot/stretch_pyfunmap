27ae56eb5b844bb8890265dd06529ef953564b62
Binit Shah <bshah@hello-robot.com>
2023-12-30 20:58:50 -0800

    Add voi side arg to head scan viz

--- src/tools/funmap_head_scan_visualizer.py
+++ src/tools/funmap_head_scan_visualizer.py
@@ -9,14 +9,16 @@ from stretch_pyfunmap.mapping import display_head_scan
 
 parser = argparse.ArgumentParser(description='Tool to create and visualize a head scan.')
 parser.add_argument("--save", nargs="?", type=str, const="",
-                    help="Save as .avi video to given filepath at end of script.")
+                    help="Save head scan to given filepath at end of script.")
+parser.add_argument("--voi-side", type=float, default=16.0,
+                    help="The length in meters for the map volume of interest.")
 args, _ = parser.parse_known_args()
 
 robot = stretch_pyfunmap.robot.FunmapRobot()
 robot.body.stow() # Reduce occlusion from the arm and gripper
 
 # Execute head scanning full procedure
-head_scanner = ma.HeadScan(robot, voi_side_m=16.0)
+head_scanner = ma.HeadScan(robot, voi_side_m=args.voi_side)
 head_scanner.execute_full()
 if args.save:
     head_scanner.save(args.save)
