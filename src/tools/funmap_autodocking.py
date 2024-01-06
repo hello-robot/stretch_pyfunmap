1b4745951d87aabd94b5aeca81bf560121beb113
Binit Shah <bshah@hello-robot.com>
2024-01-06 07:47:04 -0800

    Add left stick custom fn

--- src/tools/funmap_autodocking.py
+++ src/tools/funmap_autodocking.py
@@ -4,6 +4,7 @@ import sys
 import time
 import stretch_body.robot
 import stretch_pyfunmap.robot
+import stretch_pyfunmap.mapping as ma
 from stretch_body.gamepad_teleop import GamePadTeleop
 
 # 1. See if already connected to a charger
@@ -17,17 +18,26 @@ if body.pimu.status['charger_connected']:
 robot = stretch_pyfunmap.robot.FunmapRobot(body=body)
 gamepad_teleop = GamePadTeleop(robot_instance=False)
 gamepad_teleop.startup(robot.body)
+found_dock = False
+def step3_complete():
+    global found_dock
+    found_dock = True
+gamepad_teleop.left_stick_button_fn = step3_complete
 
 # 3. Teleop the robot to a random location within 3m of the dock
 #    and point the head cam at dock's aruco marker
 imwrite_counter = 0
-while True:
+while not found_dock:
     imwrite_counter = (imwrite_counter + 1) % 100
     if imwrite_counter == 99:
         # print("Writing Image")
         robot.detect_arucos(imwrite='/home/hello-robot/repos/stretch_pyfunmap/src/tools/view.png')
     # print(f"Charging={robot.body.pimu.status['charger_connected']}")
     gamepad_teleop.step_mainloop(robot.body)
+
+# 4. Create a head scan of the area around the docking station
+head_scanner = ma.HeadScan(robot, voi_side_m=8.0)
+
 
 gamepad_teleop.stop()
 robot.body.stop()
